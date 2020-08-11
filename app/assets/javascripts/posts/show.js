c15356a739bed45212e399f8f9999ccf0a10ff5b
LindseySawa <20betternbetter21@gmail.com>
2020-08-11 17:16:20 +0900

    adding slider and fixing whole view

---
+++ app/assets/javascripts/posts/show.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.other').slick({
+    // slidesToShow: 4,
+    // slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+    centerMode: true,
+    variableWidth: true,
+    dots: true,
+  });
+});
