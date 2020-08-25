541fb3239fdbff758dacce3634af23b28296eff0
LindseySawa <20betternbetter21@gmail.com>
2020-08-25 16:59:43 +0900

    updating files for uploading

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-    config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
+  # if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+  #   config.storage = :file
+  # elsif Rails.env.production? #本番はS3に保存する
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -29,5 +29,4 @@ CarrierWave.configure do |config|
 
     config.fog_directory  = 'onboard2020'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/onboard2020'
-  end
 end
