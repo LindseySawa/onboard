5feb85ad25f946ff3472da83a8b8057da3e5e701
LindseySawa <20betternbetter21@gmail.com>
2020-08-25 15:55:18 +0900

    modifiying settig files

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,13 +13,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
+  # storage :fog
+
 
   if Rails.env.development? || Rails.env.test?
     storage :file
   else
     storage :fog
   end
-  # storage :fog
 
 
   # Override the directory where uploaded files will be stored.
