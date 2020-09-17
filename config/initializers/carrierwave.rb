cb80b436beb3ebc7c0352e8970bc700c82c0c69f
LindseySawa <20betternbetter21@gmail.com>
2020-09-17 15:57:59 +0900

    updating carrierwave
    Updating carrierwave AGAIN!
    It didnt work even I pulled data from git

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,11 @@ CarrierWave.configure do |config|
   # if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
   #   config.storage = :file
   # elsif Rails.env.production? #本番はS3に保存する
+  if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'onboard2020'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/onboard2020'
     config.fog_credentials = {
       provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id], #credentails.ymlに鍵の本体があります
@@ -15,6 +18,9 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
 
     }
+  else
+    config.storage :file
+  end
   # config.storage = :fog
   # config.fog_provider = 'fog/aws'
   # config.fog_credentials = {
@@ -27,6 +33,4 @@ CarrierWave.configure do |config|
   #   region: 'ap-northeast-1'
   # }
 
-    config.fog_directory  = 'onboard2020'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/onboard2020'
 end
